0e5efe51f5ed26c854c426e31d0114b106ea837a
Balint Kovari <balintmaci@gmail.com>
2021-11-15 15:49:36 +0100

    created empty script for vr movement

---
+++ ARVROrigin.gd
@@ -0,0 +1,12 @@
+extends ARVROrigin
+
+onready var left = $LeftController
+onready var right = $RightController
+onready var head = $HeadCamera
+
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+   pass
