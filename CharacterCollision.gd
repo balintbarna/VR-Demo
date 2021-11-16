c620347e3735b19e6ec7a36f9bc6e7d8d8dc9b9f
Balint Kovari <balintmaci@gmail.com>
2021-11-16 02:12:26 +0100

    try to implement character collision positioning

--- CharacterCollision.gd
+++ CharacterCollision.gd
@@ -2,7 +2,42 @@ extends CollisionShape
 
 
 onready var GLOBAL_ROTATION = global_transform.basis
+onready var origin: ARVROrigin = get_origin()
+const ZERO_VECTOR = Vector3()
+
+func _physics_process(_delta: float) -> void:
+    reset_orientation()
+    set_height_between_origin_and_camera()
+    scale_to_camera_height()
 
 
-func _physics_process(delta: float) -> void:
+func set_height_between_origin_and_camera() -> void:
+    var head = get_vr_head()
+    var position = Vector3(head.global_transform.origin)
+    position.y = origin.global_transform.origin.y + head.translation.y / 2
+    global_transform.origin = position
+
+
+func reset_orientation() -> void:
     global_transform.basis = GLOBAL_ROTATION
+
+
+func scale_to_camera_height() -> void:
+    scale.y = abs(get_vr_height())
+
+
+func get_vr_height() -> float:
+    return get_vr_head().translation.y
+
+
+func get_vr_head() -> ARVRCamera:
+    return origin.head
+
+
+func get_origin() -> ARVROrigin:
+    var parent = get_parent()
+    if parent.has_method("get_origin"):
+        return parent.get_origin()
+    else:
+        push_error("No get_origin in parent")
+        return null
