93e13e030f694a493e012cb77791eabaed4089cf
Balint Kovari <balintmaci@gmail.com>
2021-11-17 17:50:22 +0100

    remove warnings

--- HmdContainer.gd
+++ HmdContainer.gd
@@ -1,7 +1,7 @@
 extends VBoxContainer
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     var head = get_hmd() as HeadCamera
     if head:
         display_transform(head)
@@ -12,8 +12,10 @@ func display_transform(head):
     var rd = head.rotation_degrees
     var posarr = ["%2.3f" % tr.x, "%2.3f" % tr.y, "%2.3f" % tr.z]
     var rotarr = ["%2.3f" % rd.x, "%2.3f" % rd.y, "%2.3f" % rd.z]
-    $TransformContainer/Position.text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
-    $TransformContainer/Rotation.text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
+    var pl = $TransformContainer/Position as Label
+    pl.text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
+    var rl = $TransformContainer/Rotation as Label
+    rl.text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
 
 
 func get_hmd() -> HeadCamera:
