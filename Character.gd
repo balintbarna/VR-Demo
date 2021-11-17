2036c5a2872d56ebce49ce4008af701865875521
Balint Kovari <balintmaci@gmail.com>
2021-11-17 15:11:40 +0100

    delete unused code

--- Character.gd
+++ Character.gd
@@ -1,17 +1,1 @@
 extends KinematicBody
-
-
-onready var origin = get_origin()
-
-
-func _physics_process(delta: float) -> void:
-    pass
-
-
-func get_origin() -> VrOrigin:
-    var parent = get_parent()
-    if parent.has_method("get_origin"):
-        return parent.get_origin()
-    else:
-        push_error("No get_origin in parent")
-        return null
