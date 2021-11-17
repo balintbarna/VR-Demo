23fb117ca1add678a609b39d2c432faf0b8f40b2
Balint Kovari <balintmaci@gmail.com>
2021-11-17 22:36:02 +0100

    have working locomotion entirely in resource

--- PlayerKinematicHandler.gd
+++ PlayerKinematicHandler.gd
@@ -1,10 +1,14 @@
 extends Resource
 class_name PlayerKinematicHandler
 
+
+const ROTATION_VECTOR = Vector3(0, -1, 0)
 const SNAP_VECTOR = Vector3(0, -1, 0)
 const UP_DIRECTION = Vector3(0, 1, 0)
 
 
+export var movement_speed = 3
+export var rotation_speed = 2*PI
 export var gravity_acceleration = 9.8
 export var stop_on_slope = true
 export var max_slides = 4
@@ -16,8 +20,44 @@ var velocity = Vector3()
 
 
 func _process(delta: float, body: KinematicBody):
+    var origin = Globals.origin as VrOrigin
+    var offset_from_rotation = apply_rotation_and_calculate_offset(delta)
+    origin.global_translate(-offset_from_rotation)
+    apply_movement(delta)
+
     velocity.y = velocity.y - gravity_acceleration * delta
     velocity = body.move_and_slide_with_snap(velocity, SNAP_VECTOR, UP_DIRECTION, stop_on_slope, max_slides, floor_max_angle, infinite_inertia)
-    var offset = body.global_transform.origin - Globals.origin.head.global_transform.origin
-    body.global_transform.origin = Globals.origin.head.global_transform.origin
-    Globals.origin.global_translate(offset)
+    var offset = body.global_transform.origin - origin.head.global_transform.origin
+    body.global_transform.origin = origin.head.global_transform.origin
+    origin.global_translate(offset)
+
+
+func apply_movement(delta: float) -> void:
+    (Globals.origin as VrOrigin).global_translate(get_complete_movement_vector(delta))
+
+
+func get_complete_movement_vector(delta: float) -> Vector3:
+    return get_movement_forward(delta) + get_movement_right(delta)
+
+
+func get_movement_forward(delta: float) -> Vector3:
+    return (Globals.origin as VrOrigin).head.get_forward_direction() * (Globals.origin as VrOrigin).left.get_movement_vector().y * delta * movement_speed
+
+
+func get_movement_right(delta: float) -> Vector3:
+    return (Globals.origin as VrOrigin).head.get_right_direction() * (Globals.origin as VrOrigin).left.get_movement_vector().x * delta * movement_speed
+
+
+func apply_rotation_and_calculate_offset(delta: float) -> Vector3:
+    var head = (Globals.origin as VrOrigin).head
+    var pos = head.global_transform.origin
+    apply_rotation(delta)
+    return head.global_transform.origin - pos
+
+
+func apply_rotation(delta: float) -> void:
+    (Globals.origin as VrOrigin).global_rotate(ROTATION_VECTOR, get_player_rotation_amount(delta))
+
+
+func get_player_rotation_amount(delta: float) -> float:
+    return (Globals.origin as VrOrigin).right.get_movement_vector().x * delta * rotation_speed
--- VrOrigin.gd
+++ VrOrigin.gd
@@ -1,56 +1,14 @@
 extends ARVROrigin
 class_name VrOrigin
 
-const MOVEMENT_SPEED = 3
-const ROTATION_SPEED = 2*PI
-const ROTATION_VECTOR = Vector3(0, -1, 0)
-
-
+# variables used elsewhere in the system
+# warning-ignore:UNUSED_CLASS_VARIABLE
 onready var left: VrController = $LeftController
+# warning-ignore:UNUSED_CLASS_VARIABLE
 onready var right: VrController = $RightController
+# warning-ignore:UNUSED_CLASS_VARIABLE
 onready var head: HeadCamera = $HeadCamera
-
-
-var freecam = true
 
 
 func _ready() -> void:
     Globals.origin = self
-
-
-func _physics_process(delta: float) -> void:
-    var offset = apply_rotation_and_calculate_offset(delta)
-    apply_movement(delta, offset)
-
-
-func apply_movement(delta: float, offset: Vector3) -> void:
-    if freecam:
-        global_translate(get_complete_movement_vector(delta) - offset)
-    else:
-        pass
-
-
-func apply_rotation_and_calculate_offset(delta: float) -> Vector3:
-    var pos = head.global_transform.origin
-    apply_rotation(delta)
-    return head.global_transform.origin - pos
-
-
-func apply_rotation(delta: float) -> void:
-    global_rotate(ROTATION_VECTOR, get_player_rotation_amount(delta))
-
-
-func get_complete_movement_vector(delta: float) -> Vector3:
-    return get_movement_forward(delta) + get_movement_right(delta)
-
-
-func get_movement_forward(delta: float) -> Vector3:
-    return head.get_forward_direction() * left.get_movement_vector().y * delta * MOVEMENT_SPEED
-
-
-func get_movement_right(delta: float) -> Vector3:
-    return head.get_right_direction() * left.get_movement_vector().x * delta * MOVEMENT_SPEED
-
-
-func get_player_rotation_amount(delta: float) -> float:
-    return right.get_movement_vector().x * delta * ROTATION_SPEED
