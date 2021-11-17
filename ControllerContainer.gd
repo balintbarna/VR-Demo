a6ad5d335574dcba7aa7217f82f258137bd335a1
Balint Kovari <balintmaci@gmail.com>
2021-11-17 17:55:49 +0100

    fix warnings

--- ControllerContainer.gd
+++ ControllerContainer.gd
@@ -28,30 +28,30 @@ func display_transform(controller):
     var rd = controller.rotation_degrees
     var posarr = ["%2.3f" % tr.x, "%2.3f" % tr.y, "%2.3f" % tr.z]
     var rotarr = ["%2.3f" % rd.x, "%2.3f" % rd.y, "%2.3f" % rd.z]
-    $LeftContainer/TransformContainer/Position.text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
-    $LeftContainer/TransformContainer/Rotation.text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
+    ($LeftContainer/TransformContainer/Position as Label).text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
+    ($LeftContainer/TransformContainer/Rotation as Label).text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
 
 
 func display_all_button_states(controller: VrController):
     var text = "All buttons:\n"
     for i in JOY_BUTTON_MAX:
         text = text + "button {}    |{}|\n".format(["%2d" % i, "X" if controller.is_button_pressed(i) else "  "], "{}")
-    $RightContainer/AllButtons.text = text
+    ($RightContainer/AllButtons as Label).text = text
 
 
 func display_all_axes_states(controller: VrController):
     var text = "All axes:\n"
     for i in JOY_AXIS_MAX:
         text = text + "axis {}    {}\n".format(["%3d" % i, "%0.2f" % controller.get_joystick_axis(i)], "{}")
-    $LeftContainer/AllAxes.text = text
+    ($LeftContainer/AllAxes as Label).text = text
 
 
 func display_basics(controller: VrController):
-    $LeftContainer/Name.text = "Name: {}".format([controller.get_name()], "{}")
-    $LeftContainer/Hand.text = "Hand: {}".format([controller.get_hand()], "{}")
-    $LeftContainer/Active.text = "Active: {}".format([controller.get_is_active()], "{}")
-    $LeftContainer/Joystick.text = "Joystick: {}".format([controller.get_joystick_id()], "{}")
-    $LeftContainer/MeshUpdate.text = "Mesh updated {} times".format([mesh_counter], "{}")
+    ($LeftContainer/Name as Label).text = "Name: {}".format([controller.get_name()], "{}")
+    ($LeftContainer/Hand as Label).text = "Hand: {}".format([controller.get_hand()], "{}")
+    ($LeftContainer/Active as Label).text = "Active: {}".format([controller.get_is_active()], "{}")
+    ($LeftContainer/Joystick as Label).text = "Joystick: {}".format([controller.get_joystick_id()], "{}")
+    ($LeftContainer/MeshUpdate as Label).text = "Mesh updated {} times".format([mesh_counter], "{}")
 
 
 func subscribe_to_controller_signals() -> void:
@@ -78,4 +78,4 @@ func _on_button_released(button: int) -> void:
     _on_button_action(button, "released")
 
 func _on_button_action(button: int, action: String) -> void:
-    $LeftContainer/ButtonEvent.text = "Button {} {}".format(["%2d" % button, action], "{}")
+    ($LeftContainer/ButtonEvent as Label).text = "Button {} {}".format(["%2d" % button, action], "{}")
--- OriginContainer.gd
+++ OriginContainer.gd
@@ -1,7 +1,7 @@
 extends VBoxContainer
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     var origin = Globals.origin as VrOrigin
     if origin:
         display_scale(origin)
@@ -9,7 +9,7 @@ func _process(delta: float) -> void:
 
 
 func display_scale(origin):
-    $WorldScale.text = "World scale: {}".format([origin.world_scale], "{}")
+    ($WorldScale as Label).text = "World scale: {}".format([origin.world_scale], "{}")
 
 
 func display_transform(origin):
@@ -17,5 +17,5 @@ func display_transform(origin):
     var rd = origin.rotation_degrees
     var posarr = ["%2.3f" % tr.x, "%2.3f" % tr.y, "%2.3f" % tr.z]
     var rotarr = ["%2.3f" % rd.x, "%2.3f" % rd.y, "%2.3f" % rd.z]
-    $TransformContainer/Position.text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
-    $TransformContainer/Rotation.text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
+    ($TransformContainer/Position as Label).text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
+    ($TransformContainer/Rotation as Label).text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
