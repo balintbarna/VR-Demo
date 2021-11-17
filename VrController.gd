be514cd17e2fefb52a43d2e034b155cfd5d3181d
Balint Kovari <balintmaci@gmail.com>
2021-11-17 21:24:12 +0100

    remove empty function

--- VrController.gd
+++ VrController.gd
@@ -6,10 +6,6 @@ const CONTROLLER_RUMBLE_FADE_SPEED = 2.0
 const CONTROLLER_DEADZONE = 0.1
 
 
-func _ready() -> void:
-    pass # Replace with function body.
-
-
 func _physics_process(delta: float) -> void:
     if rumble > 0:
         rumble -= delta * CONTROLLER_RUMBLE_FADE_SPEED
