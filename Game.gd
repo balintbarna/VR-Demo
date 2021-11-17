dff89467888dd9e629d83c61e4e6282540f72df2
Balint Kovari <balintmaci@gmail.com>
2021-11-17 14:51:38 +0100

    simplify class loading

--- Game.gd
+++ Game.gd
@@ -13,6 +13,6 @@ func load_vr_interfaces():
 
 
 func load_android_vr_interfaces():
-    var oq_success: bool = preload("res://vr-intergration/QuestConfig.gd").new().config(get_viewport())
+    var oq_success: bool = QuestConfig.new().config(get_viewport())
     if not oq_success:
         push_error("Failed to register VR interfaces for Android")
