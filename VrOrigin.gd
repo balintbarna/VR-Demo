aa8b4c87c1e69a534cfa5f2e727a5595179aa0eb
Balint Kovari <balintmaci@gmail.com>
2021-11-18 17:40:12 +0100

    spacing

--- VrOrigin.gd
+++ VrOrigin.gd
@@ -18,8 +18,10 @@ func _physics_process(_delta):
     if very_low_below_ground_level():
         reset_to_parent()
 
+
 func reset_to_parent():
     translation = Vector3()
+
 
 func very_low_below_ground_level():
     return translation.y < -50
