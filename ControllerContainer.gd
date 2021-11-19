36fe313d1be0229b21a72e90fc94b0a09167064b
Balint Kovari <balintmaci@gmail.com>
2021-11-19 18:58:05 +0100

    remove unnecessary tree traversing

--- ControllerContainer.gd
+++ ControllerContainer.gd
@@ -4,6 +4,18 @@ extends HBoxContainer
 export var controller_id: int = 0
 
 
+onready var position_label = $LeftContainer/TransformContainer/Position as Label
+onready var rotation_label = $LeftContainer/TransformContainer/Rotation as Label
+onready var buttons_label = $RightContainer/AllButtons as Label
+onready var axes_label = $LeftContainer/AllAxes as Label
+onready var name_label = $LeftContainer/Name as Label
+onready var hand_label = $LeftContainer/Hand as Label
+onready var active_label = $LeftContainer/Active as Label
+onready var joystick_label = $LeftContainer/Joystick as Label
+onready var mesh_update_label = $LeftContainer/MeshUpdate as Label
+onready var button_event_label = $LeftContainer/ButtonEvent as Label
+
+
 var mesh_counter = 0
 
 
@@ -28,30 +40,30 @@ func display_transform(controller):
     var rd = controller.rotation_degrees
     var posarr = ["%2.3f" % tr.x, "%2.3f" % tr.y, "%2.3f" % tr.z]
     var rotarr = ["%2.3f" % rd.x, "%2.3f" % rd.y, "%2.3f" % rd.z]
-    ($LeftContainer/TransformContainer/Position as Label).text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
-    ($LeftContainer/TransformContainer/Rotation as Label).text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
+    position_label.text = "Position\nX:  {}\nY:  {}\nZ: {}".format(posarr, "{}")
+    rotation_label.text = "Rotation\nX:  {}\nY:  {}\nZ: {}".format(rotarr, "{}")
 
 
 func display_all_button_states(controller: VrController):
     var text = "All buttons:\n"
     for i in JOY_BUTTON_MAX:
         text = text + "button {}    |{}|\n".format(["%2d" % i, "X" if controller.is_button_pressed(i) else "  "], "{}")
-    ($RightContainer/AllButtons as Label).text = text
+    buttons_label.text = text
 
 
 func display_all_axes_states(controller: VrController):
     var text = "All axes:\n"
     for i in JOY_AXIS_MAX:
         text = text + "axis {}    {}\n".format(["%3d" % i, "%0.2f" % controller.get_joystick_axis(i)], "{}")
-    ($LeftContainer/AllAxes as Label).text = text
+    axes_label.text = text
 
 
 func display_basics(controller: VrController):
-    ($LeftContainer/Name as Label).text = "Name: {}".format([controller.get_name()], "{}")
-    ($LeftContainer/Hand as Label).text = "Hand: {}".format([controller.get_hand()], "{}")
-    ($LeftContainer/Active as Label).text = "Active: {}".format([controller.get_is_active()], "{}")
-    ($LeftContainer/Joystick as Label).text = "Joystick: {}".format([controller.get_joystick_id()], "{}")
-    ($LeftContainer/MeshUpdate as Label).text = "Mesh updated {} times".format([mesh_counter], "{}")
+    name_label.text = "Name: {}".format([controller.get_name()], "{}")
+    hand_label.text = "Hand: {}".format([controller.get_hand()], "{}")
+    active_label.text = "Active: {}".format([controller.get_is_active()], "{}")
+    joystick_label.text = "Joystick: {}".format([controller.get_joystick_id()], "{}")
+    mesh_update_label.text = "Mesh updated {} times".format([mesh_counter], "{}")
 
 
 func subscribe_to_controller_signals() -> void:
@@ -78,4 +90,4 @@ func _on_button_released(button: int) -> void:
     _on_button_action(button, "released")
 
 func _on_button_action(button: int, action: String) -> void:
-    ($LeftContainer/ButtonEvent as Label).text = "Button {} {}".format(["%2d" % button, action], "{}")
+    button_event_label.text = "Button {} {}".format(["%2d" % button, action], "{}")
