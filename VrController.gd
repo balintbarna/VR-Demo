9a148c62242da729042d7d05b02d4343213c4349
Balint Kovari <balintmaci@gmail.com>
2021-11-19 17:50:34 +0100

    set controller mesh if framework supports it

--- VrController.gd
+++ VrController.gd
@@ -6,11 +6,25 @@ const CONTROLLER_RUMBLE_FADE_SPEED = 2.0
 const CONTROLLER_DEADZONE = 0.1
 
 
+onready var mesh_instance: MeshInstance = $MeshInstance
+
+
+func _ready():
+    var _r = connect("mesh_updated", self, "_set_controller_mesh")
+    _set_controller_mesh()
+
+
 func _physics_process(delta: float) -> void:
     if rumble > 0:
         rumble -= delta * CONTROLLER_RUMBLE_FADE_SPEED
         if rumble < 0:
             rumble = 0
+
+
+func _set_controller_mesh():
+    var mesh = get_mesh()
+    if(mesh):
+        mesh_instance.set_mesh(mesh)
 
 
 func get_biaxial_analog_input_vector() -> Vector2:
