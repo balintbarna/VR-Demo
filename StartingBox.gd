447ab8e6186b3d68f3b8aaa1277ad8d2b3113e9d
Balint Kovari <balintmaci@gmail.com>
2021-11-19 15:26:56 +0100

    added freelook portal

--- StartingBox.gd
+++ StartingBox.gd
@@ -1,15 +1,23 @@
 extends Spatial
 
 
-onready var magicportal = $MagicPortal
-onready var reflection_probe: ReflectionProbe = $ReflectionProbe
+onready var reflection_portal = $ReflectionPortal
+onready var reflection_probe = $ReflectionProbe
+onready var freemove_portal = $LocomotionPortal
 
 
 func _ready() -> void:
-    magicportal.connect("body_entered", self, "_on_body_entered_area")
+    reflection_portal.connect("body_entered", self, "_reflection_portal_activated")
+    freemove_portal.connect("body_entered", self, "_freemove_portal_activated")
 
 
-func _on_body_entered_area(body: Node):
+func _reflection_portal_activated(body: Node):
     if body is KinematicBody:
         reflection_probe.update_mode = ReflectionProbe.UPDATE_ONCE
         Globals.origin.reset_to_parent()
+
+
+func _freemove_portal_activated(body: Node):
+    if body is KinematicBody:
+        if body.has_method("switch_to_freelook_mover"):
+            body.switch_to_freelook_mover()
