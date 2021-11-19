942f7c128dd8e2ced1734edd1031af3aa3c606a7
Balint Kovari <balintmaci@gmail.com>
2021-11-19 17:25:49 +0100

    added a listing of quest buttons

---
+++ vr-intergration/QuestButtons.gd
@@ -0,0 +1,17 @@
+extends Resource
+
+const YB = 1
+const GRIP_TRIGGER = 2
+const MENU = 3
+
+const TOUCH_XA = 5
+const TOUCH_YB = 6
+const XA = 7
+
+
+const TOUCH_THUMB_UP = 10
+const INDEX_TOUCHING = 11
+const INDEX_POINTING = 12
+
+const THUMBSTICK = 14
+const INDEX_TRIGGER = 15
