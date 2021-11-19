3f149fb2332fdd47fd5aac73f24cd8cfa8e0d936
Balint Kovari <balintmaci@gmail.com>
2021-11-19 15:27:06 +0100

    character can switch to freelook

--- Character.gd
+++ Character.gd
@@ -5,5 +5,9 @@ export var kinematic_handler: Resource
 
 
 func _physics_process(delta: float):
-    var h = kinematic_handler as FlatWorldPhysicsKinematicMover
+    var h = kinematic_handler as KinematicBodyMover
     h.process(delta, self)
+
+
+func switch_to_freelook_mover():
+    kinematic_handler = FreeLookKinematicMover.new()
