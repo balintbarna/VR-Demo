11a6e12420c4ec0a336eb6fa023f3c4fb6897769
Balint Kovari <balintmaci@gmail.com>
2021-11-29 05:49:58 +0100

    have working smallportal implementation

--- portal/MagicPortal.gd
+++ portal/MagicPortal.gd
@@ -4,28 +4,53 @@ extends Spatial
 signal body_entered
 
 
-export var portal_name = "Portal"
-export var portal_color = Color(1, 1, 1, 1)
+export var portal_name = "Portal" setget set_name, get_name
+export var portal_color = Color(1, 1, 1, 1) setget set_color, get_color
 
 
 onready var collision_area: Area = $MagicArea
-onready var label = $Viewport/PortalLabel
+onready var label = $Viewport/Label
 
 
 func _ready() -> void:
     var _r = collision_area.connect("body_entered", self, "_on_body_entered_area")
-    set_properties(portal_name, portal_color)
+    update_name()
+    update_color()
 
 
 func _on_body_entered_area(body: Node):
     emit_signal("body_entered", body)
 
 
-func set_properties(n, c):
-    label.set_name(n)
-    label.set_color(c)
-    if collision_area.has_method("set_color"):
-        # warning-ignore:UNSAFE_METHOD_ACCESS
-        collision_area.set_color(c)
-    else:
-        push_error("method_not_found")
+func set_name(name):
+    portal_name = name
+    update_name()
+
+
+func get_name():
+    return portal_name
+
+
+func set_color(color):
+    portal_color = color
+    update_color()
+
+
+func get_color():
+    return portal_color
+
+
+func update_name():
+    if label:
+        label.set_name(portal_name)
+
+
+func update_color():
+    if label:
+        label.set_color(portal_color)
+    if collision_area:
+        if collision_area.has_method("set_color"):
+            # warning-ignore:UNSAFE_METHOD_ACCESS
+            collision_area.set_color(portal_color)
+        else:
+            push_error("method_not_found")
--- portal/PortalLabel.gd
+++ portal/PortalLabel.gd
@@ -1,12 +1,9 @@
-extends Control
-
-
-onready var label = ($Label as Label)
+extends Label
 
 
 func set_name(portal_name):
-    label.text = portal_name
+    text = portal_name
 
 
 func set_color(portal_color):
-    label.set("custom_colors/font_color", portal_color)
+    set("custom_colors/font_color", portal_color)
