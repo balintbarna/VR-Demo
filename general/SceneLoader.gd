dbaf619f723f7c8ecac5a5951ad4b35e7632339e
Balint Kovari <balintmaci@gmail.com>
2021-12-01 20:19:18 +0100

    implement working rudimentary async scene loading

--- general/SceneLoader.gd
+++ general/SceneLoader.gd
@@ -6,6 +6,9 @@ signal loading_complete
 
 
 var loader: ResourceInteractiveLoader = null
+var stage = 0 setget ,get_stage
+var stage_count = 0 setget ,get_stage_count
+var thread: Thread
 
 
 func _ready():
@@ -13,12 +16,19 @@ func _ready():
 
 
 func _process(_delta):
-    var r = loader.poll()
-    if r == ERR_FILE_EOF:
+    if finished():
         _on_complete()
 
 
-func get_finished():
+func get_stage():
+    return stage
+
+
+func get_stage_count():
+    return stage_count
+
+
+func finished():
     if loader:
         return not loader.get_resource() == null
     else:
@@ -37,14 +47,26 @@ func start_loading(new_loader):
         push_error("SceneLoader still working")
     else:
         loader = new_loader
-        if get_finished():
+        if finished():
             _on_complete()
         else:
+            thread = Thread.new()
+            var __ = thread.start(self, "blocking_load", Thread.PRIORITY_LOW)
             set_process(true)
         emit_signal("loading_started")
 
 
+func blocking_load(_userdata):
+    if loader:
+        var loading = true
+        while loading:
+            loading = loader.poll() == OK
+            stage = loader.get_stage()
+            stage_count = loader.get_stage_count()
+
+
 func _on_complete():
-    emit_signal("loading_complete", loader.get_resource())
+    emit_signal("loading_complete", get_scene())
     loader = null
+    thread = null
     set_process(false)
