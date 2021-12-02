7ebb1867b0996953bf08e965789ba5983e26495e
Balint Kovari <balintmaci@gmail.com>
2021-12-02 21:21:28 +0100

    make magic portal exports work better

--- portal/MagicPortal.gd
+++ portal/MagicPortal.gd
@@ -5,8 +5,8 @@ extends Spatial
 signal body_entered
 
 
-export var portal_name = "Portal" setget set_name, get_name
-export var portal_color = Color(1, 1, 1, 1) setget set_color, get_color
+export var portal_name = "Portal" setget set_portal_name, get_portal_name
+export var portal_color: Color setget set_portal_color, get_portal_color
 
 
 onready var collision_area: Area = $MagicArea
@@ -15,43 +15,33 @@ onready var label = $Viewport/Label
 
 func _ready() -> void:
     var _r = collision_area.connect("body_entered", self, "_on_body_entered_area")
-    update_name()
-    update_color()
 
 
 func _on_body_entered_area(body: Node):
     emit_signal("body_entered", body)
 
 
-func set_name(name):
-    portal_name = name
-    update_name()
-
-
-func get_name():
-    return portal_name
-
-
-func set_color(color):
-    portal_color = color
-    update_color()
-
-
-func get_color():
-    return portal_color
-
-
-func update_name():
+func set_portal_name(new_name):
     if label:
-        label.set_name(portal_name)
+        label.portal_name = new_name
 
 
-func update_color():
+func get_portal_name():
     if label:
-        label.set_color(portal_color)
+        return label.portal_name
+
+
+func set_portal_color(color):
+    if label:
+        label.portal_color = color
     if collision_area:
         if collision_area.has_method("set_color"):
             # warning-ignore:UNSAFE_METHOD_ACCESS
-            collision_area.set_color(portal_color)
+            collision_area.set_color(color)
         else:
             push_error("method_not_found")
+
+
+func get_portal_color():
+    if label:
+        return label.portal_color
--- portal/PortalLabel.gd
+++ portal/PortalLabel.gd
@@ -2,9 +2,21 @@ tool
 extends Label
 
 
-func set_name(portal_name):
-    text = portal_name
+var portal_name: String setget set_portal_name, get_portal_name
+var portal_color: Color setget set_portal_color, get_portal_color
 
 
-func set_color(portal_color):
-    set("custom_colors/font_color", portal_color)
+func set_portal_name(new_name):
+    text = new_name
+
+
+func get_portal_name():
+    return text
+
+
+func set_portal_color(new_color):
+    set("custom_colors/font_color", new_color)
+
+
+func get_portal_color():
+    return get("custom_colors/font_color")
