e7fc41d799cc1d2f6aa46c4f6137ccfa2b6ee2ea
Balint Kovari <balintmaci@gmail.com>
2021-12-02 16:04:02 +0100

    implement new grab mechanism for sword

--- sword/Sword.gd
+++ sword/Sword.gd
@@ -1,8 +1,13 @@
 extends RigidBody
 
 
-onready var grab_point = $GrabPoint as Spatial
+export var grab_handler: Resource = RigidBodyGrabHandler.new()
 
 
-func get_grab_points():
-    return [grab_point]
+func _ready():
+    grab_handler.body = self
+    grab_handler.grab_points = [$GrabPoint]
+
+
+func _physics_process(delta):
+    grab_handler.process(delta)
