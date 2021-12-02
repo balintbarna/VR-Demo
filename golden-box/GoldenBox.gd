b7be03b6c7ab6d674f3a9670fbe68b43e1c14192
Balint Kovari <balintmaci@gmail.com>
2021-12-02 22:54:21 +0100

    teleporting back and forth between rooms works

--- golden-box/GoldenBox.gd
+++ golden-box/GoldenBox.gd
@@ -4,11 +4,13 @@ extends Spatial
 onready var reflection_portal = $World/ReflectionPortal
 onready var reflection_probe = $World/ReflectionProbe
 onready var locomotion_swap_portal = $World/LocomotionPortal
+onready var portal_room_portal = $World/PortalRoomPortal
 
 
 func _ready() -> void:
     reflection_portal.connect("body_entered", self, "_reflection_portal_activated")
     locomotion_swap_portal.connect("body_entered", self, "_locomotion_swap_portal_activated")
+    portal_room_portal.connect("body_entered", self, "_portal_room_portal_activated")
 
 
 func _reflection_portal_activated(body: Node):
@@ -25,3 +27,18 @@ func _locomotion_swap_portal_activated(body: Node):
             Globals.origin.reset_to_parent()
         else:
             push_error("method_not_found")
+
+
+func _portal_room_portal_activated(body: Node):
+    if body is KinematicBody:
+        load_loading_room_scene()
+        load_portal_room_scene()
+
+
+func load_loading_room_scene():
+    if not OK == get_tree().change_scene_to(SceneLibrary.get_loading_room_scene()):
+        push_error("COULD NOT LOAD STARTING SCENE")
+
+
+func load_portal_room_scene():
+    SceneLoader.start_loading(SceneLibrary.get_portal_room_scene())
