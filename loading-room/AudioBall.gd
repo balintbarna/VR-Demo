ce8584ca625400c22e1298243f702d902dd0d622
Balint Kovari <balintmaci@gmail.com>
2021-12-02 11:30:25 +0100

    implemented dynamic grabpoint generation for audioball

---
+++ loading-room/AudioBall.gd
@@ -0,0 +1,22 @@
+extends RigidBody
+
+
+var grab_points = []
+onready var mesh_node = $MeshInstance as MeshInstance
+onready var mesh = mesh_node.mesh as SphereMesh
+
+
+func _ready():
+    for alpha in range(-180, 180, 60):
+        for beta in range(-180, 180, 60):
+            var node = Spatial.new()
+            var radius = mesh.radius
+            add_child(node)
+            grab_points.append(node)
+            var vec = Vector3(0, 0, radius).rotated(Vector3.RIGHT, deg2rad(alpha)).rotated(Vector3.UP, deg2rad(beta))
+            node.translation = vec
+            node.look_at(global_transform.origin, Vector3.UP)
+
+
+func get_grab_points():
+    return grab_points
