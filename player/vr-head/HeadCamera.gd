94c27402c5ebc1349ad291811fae86a99ea7f343
Balint Kovari <balintmaci@gmail.com>
2021-12-03 20:52:21 +0100

    add code calculating forward direction

--- player/vr-head/HeadCamera.gd
+++ player/vr-head/HeadCamera.gd
@@ -2,9 +2,30 @@ extends ARVRCamera
 class_name HeadCamera
 
 
-func get_forward_direction() -> Vector3:
-    return global_transform.basis.z.normalized() * -1
-
-
 func get_right_direction() -> Vector3:
     return global_transform.basis.x.normalized()
+
+
+func get_left_direction() -> Vector3:
+    return -get_right_direction()
+
+
+func get_up_direction() -> Vector3:
+    return global_transform.basis.y.normalized()
+
+
+func get_down_direction() -> Vector3:
+    return -get_up_direction()
+
+
+func get_backward_direction() -> Vector3:
+    return global_transform.basis.z.normalized()
+
+
+func get_forward_direction() -> Vector3:
+    return -get_backward_direction()
+
+
+func is_upright() -> bool:
+    var up_in_origin_frame = transform.basis.y.normalized()
+    return up_in_origin_frame.y > 0
--- player/vr-head/NeckPoint.gd
+++ player/vr-head/NeckPoint.gd
@@ -2,4 +2,60 @@ extends Spatial
 
 
 func _process(_delta):
-    global_transform.basis = Globals.origin.global_transform.basis
+    global_transform.basis = get_compound_frame()
+
+
+func get_compound_frame():
+    var flattened_direction_in_global_frame = get_compound_direction()
+    var rotation = ExtraMath.get_vectors_rotation(Vector3.FORWARD, flattened_direction_in_global_frame)
+    return Basis(rotation.normalized(), rotation.length())
+
+
+func get_compound_direction():
+    var head = get_head()
+    var compound = get_hands_displacement_vector() + get_hand_pair_forward_direction()
+    if head.is_upright():
+        compound += head.get_forward_direction()
+    var compound_direction_in_origin_frame = get_origin_frame().inverse() * compound
+    compound_direction_in_origin_frame.y = 0 # flatten
+    var flattened_direction_in_global_frame = get_origin_frame() * compound_direction_in_origin_frame.normalized()
+    return flattened_direction_in_global_frame
+
+
+func get_hand_pair_forward_direction():
+    # calculates the forward direction
+    # from hands relative position to each other,
+    # the right direction is the vector pointing from left hand to right,
+    # the up direction is the up direction of vr origin
+    var up_of_vr_origin_in_global = get_origin().global_transform.basis.y
+    var left_global = get_left_hand().global_transform.origin
+    var right_global = get_right_hand().global_transform.origin
+    var left_to_right_vector_in_global = right_global - left_global
+    return left_to_right_vector_in_global.rotated(up_of_vr_origin_in_global, PI/2)
+
+
+func get_hands_displacement_vector():
+    var left_global = get_left_hand().global_transform.origin
+    var right_global = get_right_hand().global_transform.origin
+    var reference_global = global_transform.origin
+    return left_global - reference_global + right_global - reference_global
+
+
+func get_origin_frame():
+    return get_origin().global_transform.basis
+
+
+func get_right_hand():
+    return get_origin().right
+
+
+func get_left_hand():
+    return get_origin().left
+
+
+func get_head():
+    return get_origin().head
+
+
+func get_origin():
+    return Globals.origin
