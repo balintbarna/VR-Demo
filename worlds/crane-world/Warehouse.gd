fe04dfd51cd557c650cee37fec7a7c2b03cfad18
Balint Kovari <balintmaci@gmail.com>
2021-12-11 02:48:26 +0100

    created dial for rotating object

--- worlds/crane-world/Warehouse.gd
+++ worlds/crane-world/Warehouse.gd
@@ -2,8 +2,10 @@ extends Spatial
 
 
 onready var target = $Target
-onready var joy = $Desk/SpringJoy1DoF
+onready var joy = $SimpleJoyDesk/SimpleJoy
+onready var dial = $DialDesk/Dial
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
     target.rotation.z = joy.angle
+    target.rotation.y = dial.angle
