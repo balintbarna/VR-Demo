0f7302a5ee334d1557766ee43474536ae66a0273
Balint Kovari <balintmaci@gmail.com>
2021-12-13 03:10:37 +0100

    added thumb support on touch screens

--- player/vr-controllers/ThumbBase.gd
+++ player/vr-controllers/ThumbBase.gd
@@ -3,6 +3,7 @@ extends Spatial
 
 onready var rest_point = $RestPoint
 onready var thumb_area = $Thumb
+onready var controller = get_parent() as VrController
 
 
 func _on_thumb_up():
@@ -11,3 +12,14 @@ func _on_thumb_up():
 
 func _on_thumb_rest():
     thumb_area.translation = rest_point.translation
+
+
+func _on_touching_area(area):
+    if controller.is_thumb_up():
+        if "touch_handler" in area:
+            area.touch_handler.on_touch(self, thumb_area)
+
+
+func _on_leaving_area(area):
+    if "touch_handler" in area:
+        area.touch_handler.on_leave(self, thumb_area)
