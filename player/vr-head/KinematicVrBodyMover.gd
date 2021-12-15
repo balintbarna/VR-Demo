e963839d6f28baab2b0ab48d7d8248b7d2784089
Balint Kovari <balintmaci@gmail.com>
2021-12-15 01:28:28 +0100

    squash bad warning

--- player/vr-head/KinematicVrBodyMover.gd
+++ player/vr-head/KinematicVrBodyMover.gd
@@ -2,6 +2,7 @@ extends VrBodyManipulator
 class_name KinematicVrBodyMover
 
 
+# warning-ignore:UNSAFE_CAST
 onready var kinematic_body = body as KinematicBody
 func _ready():
     if not kinematic_body is KinematicBody:
