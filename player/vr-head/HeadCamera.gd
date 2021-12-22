0a604f43661fba1a58c4e86956b307ea48444316
Balint Kovari <balintmaci@gmail.com>
2021-12-22 23:07:01 +0100

    pitch hmd if not arvr mode

--- player/vr-head/HeadCamera.gd
+++ player/vr-head/HeadCamera.gd
@@ -2,6 +2,14 @@ extends ARVRCamera
 class_name HeadCamera
 
 
+export var mouse_pitch_speed = 2*PI
+
+
+func _physics_process(delta):
+    if not ARVRServer.primary_interface:
+        self.rotate_x(Input.get_axis("pitch_down", "pitch_up") * delta * mouse_pitch_speed)
+
+
 func get_right_direction() -> Vector3:
     return global_transform.basis.x.normalized()
 
