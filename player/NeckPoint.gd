4318867c59269e9bf3e1542ab2f556abcec2e5b1
Balint Kovari <balintmaci@gmail.com>
2021-12-31 01:39:09 +0100

    simplify height setting on neckpoint

--- player/NeckPoint.gd
+++ player/NeckPoint.gd
@@ -5,5 +5,9 @@ onready var feet_point = $ChestPoint/StomachPoint/GenitaliaPoint/KneeHeight/Feet
 onready var default_height = abs((global_transform.orthonormalized().inverse() * feet_point.global_transform.origin).y)
 
 
+func _physics_process(_delta):
+    self.set_height(self.translation.y)
+
+
 func set_height(value):
     scale.y = value / default_height
