ff4d854f18b3f342f701268a892c715007d6d112
Balint Kovari <balintmaci@gmail.com>
2021-12-31 15:44:34 +0100

    added some generic unit tests about the type system

--- test/unit/test_generic.gd
+++ test/unit/test_generic.gd
@@ -1,6 +1,25 @@
 extends "res://addons/gut/test.gd"
 
 
+func test_types():
+    var node_type = ArvrOriginWithReferences
+    var node = ArvrOriginWithReferences.new()
+    assert_true(node is node_type)
+    node.free()
+    assert_false(match_type())
+
+
+func match_type():
+    var node_type = ArvrOriginWithReferences
+    var node = ArvrOriginWithReferences.new()
+    var result = false
+    match node:
+        node_type:
+            result = true
+    node.free()
+    return result
+
+
 func test_string_truthy():
     var s: String
     assert_falsy(s)
